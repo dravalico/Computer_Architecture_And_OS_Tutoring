40964c5fd36290bfe2fbcde442718d49c52911d9
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-04-14 11:12:41 +0200

    Some fixes

--- tutoring_02/solutions/assembly/exercise06_solution.s
+++ tutoring_02/solutions/assembly/exercise06_solution.s
@@ -1,15 +1,15 @@
 		MOV		R1, #4 ; dimensione vettore
 		MOV		R2, #0x100 ; indirizzo base per salvare vettore
-		MOV		R3, #12 ; primo valore da salvare
+		MOV		R3, #-12 ; primo valore da salvare
 		STR		R3, [R2, #0] ; salvo il primo valore in 0x100
 		MOV		R3, #-4 ; secondo valore ecc..
 		STR		R3, [R2, #4]
-		MOV		R3, #52
+		MOV		R3, #2
 		STR		R3, [R2, #8]
-		MOV		R3, #-40
+		MOV		R3, #40
 		STR		R3, [R2, #12]
 		MOV		R3, #0 ; j = 0
-		MOV		R4, #0 ; registro per leggere vettore
+		MOV		R4, #0 ; registro per leggere vettore, i = 0
 		LDR		R0, [R2, R4] ; carico il primo valore
 		ADD		R4, R4, #4 ; i parte dal secondo valore
 loop
@@ -18,7 +18,7 @@ loop
 		LDR		R5, [R2, R4] ; carico myArray[i]
 		CMP		R5, R0 ; confronto con il valore maggiore attuale
 		BLT		next ; se non è maggiore, vado avanti col ciclo
-		MOV		R0, R5 ; se è maggiore, aggionrno R0
+		MOV		R0, R5 ; se è maggiore, aggiorno R0
 next
 		ADD		R3, R3, #1 ; incremento j
 		ADD		R4, R4, #4 ; incremento i
